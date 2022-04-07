a3122983382917d8894f721c96ce903cc952aeca
i-am-sam-codes <sammmceja@gmail.com>
2022-04-07 11:10:42 -0700

    Typos & Adjustments

--- myproject/main.js
+++ myproject/main.js
@@ -3,8 +3,7 @@ import './style.css'
 import * as THREE from 'three';
 //will allows us to move around the scene with out using our mouse 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { ShadowMaterial } from 'three';
-import { CameraHelper } from 'three';
+
 
 const scene = new THREE.Scene();
 
@@ -13,9 +12,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 //renderer to render the graphics to the scene 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
-})
+});
 
 renderer.setPixelRatio( window.devicePixelRatio );
 
@@ -24,32 +23,33 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 //moving camera to the z axis 
 camera.position.setZ(30);
+camera.position.setX(-3);
 
-renderer.render( scene, camera )
+renderer.render( scene, camera );
 
 //There are 3 basic steps to creating an object. 
 // #1 Need geometry/vector define the object. THREE.js has a bunch of build in geometrys. 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
 // #2 Need to define material to define color or texture. Theres many different materials to chose from with THREE.js material
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
+const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } )
 // #3 Create a MESH geometry + material combining them both together
-const torus = new THREE.Mesh( geometry, material )
+const torus = new THREE.Mesh( geometry, material );
 //and add MESH to scene 
-scene.add( torus )
+scene.add( torus );
 
-//add lighting - pointLight is an easy light which emits light in all directions like a lightbulb 
+// Add Lights - pointLight is an easy light which emits light in all directions like a lightbulb 
 const pointLight = new THREE.PointLight(0xffffff)
 //setting the light away from center 
-pointLight.position.set(20, 20, 20)
+pointLight.position.set(5, 5, 5)
 
 //ambient light adds light to everything equally 
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 //lightHelper shows us the position of the point light
-const lightHelper = new THREE.PointLightHelper(pointLight)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
 //draws a two dimentional grid to the scene 
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lightHelper, gridHelper)
+// const gridHelper = new THREE.GridHelper(200, 50)
+// scene.add(lightHelper, gridHelper)
 
 
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -112,7 +112,7 @@ const sam = new THREE.Mesh(
   new THREE.MeshBasicMaterial( { map: samTexture } )
 )
 
-scene.add(sam)
+scene.add(sam);
 
 // Moon 
 const moonTexture = new THREE.TextureLoader().load('moonmoon.jpg');
@@ -130,3 +130,7 @@ scene.add(moon)
 //reposition moon 
 moon.position.z = 30
 moon.position.setX(-10);
+
+
+sam.position.z = -5;
+sam.position.x = 2;
